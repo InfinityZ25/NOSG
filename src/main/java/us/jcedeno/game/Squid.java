10ef0112682f0f371f3df1bb2f3a42d65b3689c1
Juan Cedeno (MBPro) <henixceo@gmail.com>
2022-03-23 00:43:49 -0400

    wip, tired

--- src/main/java/us/jcedeno/game/Squid.java
+++ src/main/java/us/jcedeno/game/Squid.java
@@ -10,18 +10,25 @@ import lombok.Getter;
 import me.lofro.core.paper.data.LocationSerializer;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
+/**
+ * Entrypoint for Not Otaku SquidGame plugin.
+ * 
+ * 
+ * @author <a href="https://github.com/zLofro">lofro</a> - Developer.
+ * 
+ * @author <a href="https://jcedeno.us/github">jcedeno</a> - Consultant &
+ *         Collaborator.
+ */
 public class Squid extends JavaPlugin {
-
     private @Getter static Squid instance;
-
-    private final @Getter static MiniMessage miniMessage = MiniMessage.miniMessage();
-    private @Getter static final Gson gson = new GsonBuilder()
+    private static final MiniMessage miniMessage = MiniMessage.miniMessage();
+    /**  GSON instance with custom serializers & config */
+    private static final Gson gson = new GsonBuilder()
             .registerTypeAdapter(Location.class, new LocationSerializer())
             .registerTypeAdapter(Location[].class, LocationSerializer.getArraySerializer())
             .setPrettyPrinting()
             .serializeNulls()
             .create();
-            
 
     @Override
     public void onEnable() {
@@ -32,6 +39,24 @@ public class Squid extends JavaPlugin {
     @Override
     public void onDisable() {
 
+    }
+
+    /**
+     * A static method to aceess the Gson object globally.
+     * 
+     * @return gson with all adapter types registered.
+     */
+    public static Gson gson() {
+        return gson;
+    }
+
+    /**
+     * A static method to access the MiniMessage object globally.
+     * 
+     * @return MiniMessage object.
+     */
+    public static MiniMessage miniMessage() {
+        return miniMessage;
     }
 
 }
---
+++ src/main/java/us/jcedeno/game/players/commands/RoleManagerCMD.java
@@ -0,0 +1,5 @@
+package us.jcedeno.game.players.commands;
+
+public class RoleManagerCMD {
+    
+}
