71927fb5fd8278907d9ef443961a5e0e73edc3cc
Juan Cedeno (MBPro) <henixceo@gmail.com>
2022-03-23 13:42:01 -0400

    added more comments

--- src/main/java/us/jcedeno/game/players/commands/RoleManagerCMD.java
+++ src/main/java/us/jcedeno/game/players/commands/RoleManagerCMD.java
@@ -1,5 +1,16 @@
 package us.jcedeno.game.players.commands;
 
-public class RoleManagerCMD {
-    
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Subcommand;
+
+/**
+ * A command to manually change & query the roles of players.
+ * 
+ * @author jcedeno
+ */
+@CommandAlias("squid role")
+@Subcommand("role")
+public class RoleManagerCMD extends BaseCommand {
+
 }
