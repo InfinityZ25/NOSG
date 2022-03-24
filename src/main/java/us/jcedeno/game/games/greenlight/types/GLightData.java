b7bead6a940da3d7b81cc8c4b40484202cc30808
Juan Cedeno (MBPro) <henixceo@gmail.com>
2022-03-23 22:31:17 -0400

    added cannons

--- src/main/java/us/jcedeno/game/games/greenlight/types/GLightData.java
+++ src/main/java/us/jcedeno/game/games/greenlight/types/GLightData.java
@@ -1,5 +1,8 @@
 package us.jcedeno.game.games.greenlight.types;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Location;
 
 import lombok.Getter;
@@ -14,9 +17,21 @@ public class GLightData {
 
     @Getter
     Location cuberUpper, cubeLower;
+    @Getter
+    List<Location> cannonLocations;
 
-    public GLightData(Location cuberUpper, Location cubeLower) {
+    public GLightData(Location cuberUpper, Location cubeLower, Location... cannonLocations) {
         this.cuberUpper = cuberUpper;
         this.cubeLower = cubeLower;
+        this.cannonLocations = new ArrayList<>();
+        for (Location loc : cannonLocations) {
+            this.cannonLocations.add(loc);
+        }
+    }
+
+    public GLightData(Location cubeUpper, Location cubeLower, List<Location> cannonLocations) {
+        this.cuberUpper = cubeUpper;
+        this.cubeLower = cubeLower;
+        this.cannonLocations = cannonLocations;
     }
 }
