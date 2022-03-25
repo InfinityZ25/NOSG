6228099369ffc805da6da243ec465355360fa667
Juan Cedeno (MBPro) <henixceo@gmail.com>
2022-03-25 01:12:03 -0400

    fixed typo

--- src/main/java/us/jcedeno/game/global/listeners/GlobalListener.java
+++ src/main/java/us/jcedeno/game/global/listeners/GlobalListener.java
@@ -1,8 +1,5 @@
 package us.jcedeno.game.global.listeners;
 
-import io.papermc.paper.event.player.AsyncChatEvent;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -17,6 +14,10 @@ import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.potion.PotionEffectType;
+
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import us.jcedeno.game.games.GameManager;
 import us.jcedeno.game.global.utils.Strings;
 import us.jcedeno.game.global.utils.datacontainers.Data;
@@ -61,7 +62,8 @@ public class GlobalListener implements Listener {
 
         e.quitMessage(null);
 
-        pManager.guardMessage(Component.text(Strings.format("&7El jugador &8" + name + " &7ha abandonado el servidor.")));
+        pManager.guardMessage(
+                Component.text(Strings.format("&7El jugador &8" + name + " &7ha abandonado el servidor.")));
 
         var timer = gManager.getBukkitTimer();
         timer.removeViewer(player);
@@ -73,8 +75,9 @@ public class GlobalListener implements Listener {
 
         e.setCancelled(true);
 
-        if (pManager.isGuard(player)) pManager.guardMessage(Strings.componentFormat("&cGUARDS &8| &7" + player.getName() + " &8| &8&l>> &7"
-                + PlainTextComponentSerializer.plainText().serialize(e.message())));
+        if (pManager.isGuard(player))
+            pManager.guardMessage(Strings.componentFormat("&cGUARDS &8| &7" + player.getName() + " &8| &8&l>> &7"
+                    + PlainTextComponentSerializer.plainText().serialize(e.message())));
     }
 
     @EventHandler
@@ -86,29 +89,34 @@ public class GlobalListener implements Listener {
 
         try {
             PersistentDataContainer dataContainer = Data.getData(player);
-            Data.set(dataContainer, "DEATH_LOCATION", gManager.getSquidInstance(), PersistentDataType.INTEGER_ARRAY, new int[]{deathLocation.getBlockX(), deathLocation.getBlockY(), deathLocation.getBlockZ()});
-            Data.set(dataContainer, "DEATH_LOCATION_ROTATION", gManager.getSquidInstance(), PersistentDataType.FLOAT, player.getLocation().getYaw());
+            Data.set(dataContainer, "DEATH_LOCATION", gManager.getSquidInstance(), PersistentDataType.INTEGER_ARRAY,
+                    new int[] { deathLocation.getBlockX(), deathLocation.getBlockY(), deathLocation.getBlockZ() });
+            Data.set(dataContainer, "DEATH_LOCATION_ROTATION", gManager.getSquidInstance(), PersistentDataType.FLOAT,
+                    player.getLocation().getYaw());
         } catch (PlayerIsNotOnlineException ex) {
             ex.printStackTrace();
         }
 
         e.deathMessage(null);
 
-        if (!pManager.isPlayer(player) || player.getGameMode().equals(GameMode.SPECTATOR)) return;
+        if (!pManager.isPlayer(player) || player.getGameMode().equals(GameMode.SPECTATOR))
+            return;
 
         var squidPlayer = pManager.pData().getPlayer(name);
         int playerID = squidPlayer.getId();
 
         player.setGameMode(GameMode.SPECTATOR);
-        if (squidPlayer.isDead()) return;
+        if (squidPlayer.isDead())
+            return;
 
         squidPlayer.setDead(true);
 
-        Bukkit.getOnlinePlayers().forEach(online -> online.playSound(online.getLocation(), "sfx.elimination", 1,1));
+        Bukkit.getOnlinePlayers().forEach(online -> online.playSound(online.getLocation(), "sfx.elimination", 1, 1));
 
-        Bukkit.broadcast(Component.text(me.lofro.core.paper.utils.strings.Strings.format("&bEl jugador &3#" + playerID + " " + name + " &bha sido eliminado.")));
+        Bukkit.broadcast(
+                Component.text(Strings.format("&bEl jugador &3#" + playerID + " " + name + " &bha sido eliminado.")));
 
-        //TODO CABEZA.
+        // TODO CABEZA.
     }
 
     @EventHandler
@@ -117,9 +125,12 @@ public class GlobalListener implements Listener {
 
         try {
             PersistentDataContainer persistentDataContainer = Data.getData(player);
-            int[] locationBlocks = Data.get(persistentDataContainer,"DEATH_LOCATION",gManager.getSquidInstance(), PersistentDataType.INTEGER_ARRAY);
-            float yaw = Data.get(persistentDataContainer, "DEATH_LOCATION_ROTATION", gManager.getSquidInstance(), PersistentDataType.FLOAT);
-            Location respawnLocation = new Location(player.getWorld(), locationBlocks[0], locationBlocks[1], locationBlocks[2]);
+            int[] locationBlocks = Data.get(persistentDataContainer, "DEATH_LOCATION", gManager.getSquidInstance(),
+                    PersistentDataType.INTEGER_ARRAY);
+            float yaw = Data.get(persistentDataContainer, "DEATH_LOCATION_ROTATION", gManager.getSquidInstance(),
+                    PersistentDataType.FLOAT);
+            Location respawnLocation = new Location(player.getWorld(), locationBlocks[0], locationBlocks[1],
+                    locationBlocks[2]);
             respawnLocation.setYaw(yaw);
 
             e.setRespawnLocation(respawnLocation);
@@ -131,10 +142,9 @@ public class GlobalListener implements Listener {
     @EventHandler
     public void onFood(FoodLevelChangeEvent e) {
         Player player = (Player) e.getEntity();
-        if(!player.hasPotionEffect(PotionEffectType.SATURATION) && !player.hasPotionEffect(PotionEffectType.HUNGER)){
+        if (!player.hasPotionEffect(PotionEffectType.SATURATION) && !player.hasPotionEffect(PotionEffectType.HUNGER)) {
             e.setCancelled(true);
         }
     }
-
 
 }
